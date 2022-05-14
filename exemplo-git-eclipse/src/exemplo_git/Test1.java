49b51a67f1445aa6d278ac3d5615aa50c1a95f9b
walis <walis@DESKTOP-9RCGHUQ>
2022-05-14 12:22:37 -0300

    Alteração feita pelo eclipse.

--- exemplo-git-eclipse/src/exemplo_git/Test1.java
+++ exemplo-git-eclipse/src/exemplo_git/Test1.java
@@ -4,6 +4,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		System.out.println("ola github!");
+		System.out.println("Bem-Vindo ao github");
+		System.out.println("Alteração feita pelo eclipse");
 	}
 
 }
